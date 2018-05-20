d028b6076d40d488a5cb1bf8f94b78be9708da0f
AxelBoisson <36669790+AxelBoisson@users.noreply.github.com>
2018-05-20 10:27:59 +0200

    Spy + Moat OK

--- dominion/dominion/card/base/Spy.java
+++ dominion/dominion/card/base/Spy.java
@@ -53,17 +53,37 @@ public class Spy extends AttackCard {
 			}
 				
 			String decision;
-			decision = p.choose("Voulez vous défaussez cette carte ou la replacer sur son deck ?", choice, false);
-			// Si le joueur souhaite défausser cette carte
-			if(decision.equalsIgnoreCase("y")) {
-				playerGame.get(i).removeDraw(firstCardPlayer.getName()); // On enleve la carte de la pioche de l'adversaire
-				playerGame.get(i).addDiscard(firstCardPlayer);	// On ajoute la carte dans sa défausse
+			// Si l'adversaire a dans sa main une carte Moat
+			if(playerGame.get(i).cardsInHand().getCard("Moat") != null) {
+				// On appelle la fonction reaction de la carte qui va demander a l'adversaire si il veut utiliser sa carte Moat
+				if(!((Moat) playerGame.get(i).cardsInHand().getCard("Moat")).reaction(playerGame.get(i))) {
+					decision = p.choose("Voulez vous défaussez cette carte ou la replacer sur son deck ?", choice, false);
+					// Si le joueur souhaite défausser cette carte
+					if(decision.equalsIgnoreCase("y")) {
+						playerGame.get(i).removeDraw(firstCardPlayer.getName()); // On enleve la carte de la pioche de l'adversaire
+						playerGame.get(i).addDiscard(firstCardPlayer);	// On ajoute la carte dans sa défausse
+					}
+					
+					// Si le joueur choisit non et que ce n'est pas le joueur qui a joué la carte Spy
+					if(decision.equalsIgnoreCase("n") && !playerGame.get(i).equals(playerGame.get(0))){
+						playerGame.get(i).addDraw(firstCardPlayer); // On remet la carte dans la pioche
+					}
+				}
 			}
-			
-			// Si le joueur choisit non et que ce n'est pas le joueur qui a joué la carte Spy
-			if(decision.equalsIgnoreCase("n") && !playerGame.get(i).equals(playerGame.get(0))){
-				playerGame.get(i).addDraw(firstCardPlayer); // On remet la carte dans la pioche
+			else {
+				decision = p.choose("Voulez vous défaussez cette carte ou la replacer sur son deck ?", choice, false);
+				// Si le joueur souhaite défausser cette carte
+				if(decision.equalsIgnoreCase("y")) {
+					playerGame.get(i).removeDraw(firstCardPlayer.getName()); // On enleve la carte de la pioche de l'adversaire
+					playerGame.get(i).addDiscard(firstCardPlayer);	// On ajoute la carte dans sa défausse
+				}
+				
+				// Si le joueur choisit non et que ce n'est pas le joueur qui a joué la carte Spy
+				if(decision.equalsIgnoreCase("n") && !playerGame.get(i).equals(playerGame.get(0))){
+					playerGame.get(i).addDraw(firstCardPlayer); // On remet la carte dans la pioche
+				}
 			}
+				
 			
 			
 				
